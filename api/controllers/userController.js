9f203ea8f777f967764790d7f3287a4d94865960
AtsuyaSakamoto <sakamo.070501@gmail.com>
2021-10-04 18:14:48 +0900

    ライブラリ変更  bcrypt→bcryptjs

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,5 @@
 require('express')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const saltRounds = 10
 const User = require('../models/user')
